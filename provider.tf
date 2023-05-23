31f317cfa43cc1378eccd8742f248d843f0917fa
Jose Noronha <jnoronha@uolinc.com>
2023-05-23 15:18:28 -0300

    feat(provider): definições básicas de provider e tags

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~>4.67.0" }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "aws-solutions-architect"
+}
+
+variable "tags" {
+  type    = object({ project = string })
+  default = { project = "aws-solutions-architect" }
+}
