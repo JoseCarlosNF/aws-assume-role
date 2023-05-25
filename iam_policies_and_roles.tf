6b33085f197514745de184b86588d69194903f5a
Jose Noronha <jnoronha@uolinc.com>
2023-05-25 11:34:29 -0300

    feat(policies/roles): adiciona policies e roles
    Permitem o acesso aos buckets específicos por cargo. Podendo ser admin ou developer.

---
+++ iam_policies_and_roles.tf
@@ -0,0 +1,124 @@
+# -------------------------------- Policies ----------------------------------
+resource "aws_iam_policy" "AppConfig" {
+  name        = "S3AppConfig"
+  description = "Define policies to access appconfig bucket."
+  tags        = var.tags
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "ListAllBuckets"
+        Effect   = "Allow"
+        Action   = ["s3:ListAllMyBuckets"]
+        Resource = ["arn:aws:s3:::*"]
+      },
+      {
+        Sid    = "AllowAccessObjects"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+        ]
+        Resource = [
+          aws_s3_bucket.appconfig.arn,
+          "${aws_s3_bucket.appconfig.arn}/*",
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "CustomerData" {
+  name        = "S3CustomerData"
+  description = "Define policies to access customerdata bucket."
+  tags        = var.tags
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "ListAllBuckets"
+        Effect   = "Allow"
+        Action   = ["s3:ListAllMyBuckets"]
+        Resource = ["arn:aws:s3:::*"]
+      },
+      {
+        Sid    = "AllowAccessObjects"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+        ]
+        Resource = [
+          aws_s3_bucket.customerdata.arn,
+          "${aws_s3_bucket.customerdata.arn}/*",
+        ]
+      }
+    ]
+  })
+}
+
+# ---------------------------------- Roles ------------------------------------
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "S3Developers" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "AWS"
+      identifiers = concat(
+        [data.aws_caller_identity.current.account_id],
+        [for user in local.groups.developers : aws_iam_user.users[user].arn]
+      )
+    }
+  }
+}
+
+data "aws_iam_policy_document" "S3Admins" {
+  statement {
+    sid     = "AllowAdminsAccessS3CustomerData"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "AWS"
+      identifiers = concat(
+        [data.aws_caller_identity.current.account_id],
+        [for user in local.groups.admins : aws_iam_user.users[user].arn],
+      )
+    }
+  }
+}
+
+resource "aws_iam_role" "S3Developers" {
+  name               = "S3Developers"
+  tags               = var.tags
+  assume_role_policy = data.aws_iam_policy_document.S3Developers.json
+}
+
+resource "aws_iam_role" "S3Admins" {
+  name               = "S3Admins"
+  tags               = var.tags
+  assume_role_policy = data.aws_iam_policy_document.S3Admins.json
+}
+
+output "roles" {
+  value = {
+    developers = aws_iam_role.S3Developers.name
+    admins = aws_iam_role.S3Admins.name
+  }
+}
+
+# ---------------------- Attachment Policies on Roles ------------------------
+resource "aws_iam_role_policy_attachment" "S3Developers" {
+  role       = aws_iam_role.S3Developers.name
+  policy_arn = aws_iam_policy.AppConfig.arn
+}
+
+resource "aws_iam_role_policy_attachment" "S3AdminsAppConfig" {
+  role       = aws_iam_role.S3Admins.name
+  policy_arn = aws_iam_policy.AppConfig.arn
+}
+
+resource "aws_iam_role_policy_attachment" "S3AdminsCustomerData" {
+  role       = aws_iam_role.S3Admins.name
+  policy_arn = aws_iam_policy.CustomerData.arn
+}
